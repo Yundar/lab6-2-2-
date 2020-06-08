f03677ab2dfdec18d517c064b5caa918f63d8d77
Yurii <Yurri2002@gmail.com>
2020-06-08 18:01:37 +0300

    wda

--- main.py
+++ main.py
@@ -26,33 +26,34 @@ def fib(value, array, index):
 class Array:
     def __init__(self, length):
         self.length = length
+        self.array = list()
 
     def generate_array(self):
-        array = list()
-        while len(array) != self.length:
+        l = 0
+        while l != self.length:
             i = random.randrange(self.length*10)
-            if i not in array:
-                array.append(i)
-        array.sort()
-        return array
+            if i not in self.array:
+                self.array.append(i)
+                l += 1
+        self.array.sort()
 
-    def fib(self, value, array, index):
+    def fib(self, value, index):
         fibonachi = [1, 2]
         indices_fib = [0]
-        for i in range(len(array)):
+        for i in range(len(self.array)):
             fibonachi.append(fibonachi[-2] + fibonachi[-1])
-            if fibonachi[i - indices_fib[-1]] - 1 < len(array):
-                if value == array[fibonachi[i - indices_fib[-1]] - 1]:
+            if fibonachi[i - indices_fib[-1]] - 1 < len(self.array):
+                if value == self.array[fibonachi[i - indices_fib[-1]] - 1]:
                     index += fibonachi[i - indices_fib[-1]] - 1
                     print('index = ' + str(index))
                     break
-                elif value > array[fibonachi[i - indices_fib[-1]] - 1]:
+                elif value > self.array[fibonachi[i - indices_fib[-1]] - 1]:
                     continue
-                elif value < array[fibonachi[i - indices_fib[-1]] - 1]:
-                    array = array[fibonachi[i - 1 - indices_fib[-1]]:fibonachi[i]]
+                elif value < self.array[fibonachi[i - indices_fib[-1]] - 1]:
+                    self.array = self.array[fibonachi[i - 1 - indices_fib[-1]]:fibonachi[i]]
                     index += fibonachi[i - 1 - indices_fib[-1]]
                     indices_fib.append(i)
             else:
-                array = array[fibonachi[i - 1 - indices_fib[-1]]::]
+                self.array = self.array[fibonachi[i - 1 - indices_fib[-1]]::]
                 index += fibonachi[i - 1 - indices_fib[-1]]
-                fib(value, array, index)
+                fib(value, self.array, index)
